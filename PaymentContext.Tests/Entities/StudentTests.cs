c72ea802c6418f055cd1aff11f0a3152ad21551b
mtswill <matheuswillian91@hotmail.com>
2021-08-19 20:46:57 -0300

    definindo entidades

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void AdicionarAssinatura()
+        {
+            var subscription = new Subscription(null);
+            var student = new Student(
+                "Matheus",
+                "Polato",
+                "123456789",
+                "mtswill@mtswill.com"
+            );
+            student.AddSubscription(subscription);
+        }
+    }
+}
